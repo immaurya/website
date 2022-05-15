4f38f71cb0800035bb8fc33548b941989fb9325b
immaurya <minimaurya171996@gmail.com>
2022-05-15 12:01:03 +0000

    modified:   ms.java

--- ms.java
+++ ms.java
@@ -3,7 +3,7 @@ public class ms {
     public static void main(String[] args){
 
 	for(int i=1;i<=3;i++) {
-       System.out.println("Hello MS! Welcome to the test session"+i);
+       System.out.println("Hello MS! Welcome to the training session"+i);
  
 }
 
