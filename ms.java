1887faaa6db0ead4bb7124b21dccee34d0aeff3d
immaurya <minimaurya171996@gmail.com>
2022-05-15 11:34:47 +0000

    new file:   ms.java

---
+++ ms.java
@@ -0,0 +1,11 @@
+public class ms {
+
+    public static void main(String[] args){
+
+	for(int i=1;i<=3;i++) {
+       System.out.println("Hello MS! Welcome to the test session"+i);
+ 
+}
+
+     }
+}
