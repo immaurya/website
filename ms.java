a43443027fc65d145f383f4d0176b3a874f96182
immaurya <minimaurya171996@gmail.com>
2022-05-18 04:30:19 +0000

    modified:   website (modified content)

--- ms.java
+++ ms.java
@@ -3,7 +3,7 @@ public class ms {
     public static void main(String[] args){
 
 	for(int i=1;i<=5;i++) {
-       System.out.println("Hello MS! Welcome to the training session"+i);
+       System.out.println("Hello MS! Welcome to the CI/CD training session"+i);
  
 }
 
